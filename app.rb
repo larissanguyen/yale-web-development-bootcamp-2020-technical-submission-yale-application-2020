917039aa3b74c9964d26792e28b01c381dfcacec
Larissa Nguyen <larissa.nguyen@yale.edu>
2020-05-08 18:18:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,7 +4,9 @@ AVERAGE_HEIGHT_IN_INCHES = 67
 # Your Code Here!
 name = 'Larissa'
 age = 21
-
+favorite_food = "cereal"
+height_in_inches = 64
+height_message = (height_in_inches > AVERAGE_HEIGHT_IN_INCHES) ? "You are taller than average."
 
 ## Do not modify below this line!
 
